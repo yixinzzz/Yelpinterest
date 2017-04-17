fa5223f32cab4a7cdff9678fa10db8e21c6fae90
Yixin Zhang <amiee@Yixins-MacBook-Pro.local>
2017-04-16 23:22:06 -0700

    v1.0

---
+++ pull_data.py
@@ -0,0 +1,110 @@
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+from bs4 import BeautifulSoup
+import urllib
+import re
+
+def connect_API(consumer_key=None, consumer_secret=None, token=None, token_secret=None):
+    """ Yelp API authorization
+    :param consumer_key: Yelp API consumer_key
+    :param consumer_secret: Yelp API consumer_secret
+    :param token: Yelp API token
+    :param token_secret: Yelp API token_secret
+    :return: Yelp API client object
+    """
+    auth = Oauth1Authenticator(consumer_key, consumer_secret, token, token_secret)
+    return Client(auth)
+
+def get_businesses(client, keywords, location):
+    """
+    get Yelp business objects
+    :param client: Yelp API client object
+    :param keywords: name of a dish
+    :param location: city name
+    :return: a list of Yelp business object from responses
+    """
+    params = {
+        'term': keywords
+    }
+    return client.search(location, **params).businesses
+
+def get_data(business_id, keywords):
+    """
+    pull reviews and photos
+    :param business_id: a Yelp business id
+    :param keywords: name of a dish
+    :return: a list of reviews and a list of photos relative to the keywords
+    """
+
+    # data in 1st page
+    reviews = []
+    photos = []
+    SearchURL = 'https://www.yelp.com/biz_photos/%s?tab=food' % business_id
+    try:
+        yelppage = BeautifulSoup(urllib.urlopen(SearchURL).read(), "lxml")
+        total_pages = yelppage.find_all('div', {'class': "page-of-pages arrange_unit arrange_unit--fill"})[0].get_text()
+        total_pages = int(re.findall('of ([0-9]*)', total_pages)[0])
+        reviews, photos = get_data_single_page(yelppage, keywords)
+        # data in other pages (only get data from first 5 pages; otherwise, it's going to be super slow)
+        if total_pages > 5:
+            total_pages = 5
+        for page in range(1, total_pages):
+            num = 30*page
+            SearchURL = 'https://www.yelp.com/biz_photos/%s?start=%s&tab=food' % (business_id, str(num))
+            yelppage = BeautifulSoup(urllib.urlopen(SearchURL).read(), "lxml")
+            r, p = get_data_single_page(yelppage, keywords)
+            reviews += r
+            photos += p
+    except UnicodeError:
+        pass
+    return reviews, photos
+
+def get_data_single_page(yelppage, keywords):
+    """
+    helper method for get_data
+    :param yelppage: BeautifulSoup object
+    :return: a list of reviews and a list of photos relative to the keywords
+    """
+    results = yelppage.find_all('img', {'alt': re.compile(" " + keywords + " ", re.IGNORECASE)})
+    reviews = []
+    photos = []
+    for result in results:
+        r = result.get("alt").split(".")
+        if len(r) > 1:
+            reviews.append(r[1])
+            photos.append(result.get("src"))
+    return reviews, photos
+
+def find_food(keywords, location):
+    """
+    get restaurant information from searching results
+    :param keywords: keywords for the dish
+    :param location: city name
+    :return: a dictionary of result (key - business_id, value - review list, photo list, business object)
+    """
+
+    client = connect_API(consumer_key, consumer_secret, token, token_secret)
+    businesses_list = get_businesses(client, keywords, location)
+
+    info_dict = {}
+    for business in businesses_list[:1]:
+        id = business.id
+        # print id
+        reviews, photos = get_data(id, keywords)
+        if len(reviews) > 0:
+            info_dict[id] = {"reviews": reviews}
+            info_dict[id]["photos"] = photos
+            info_dict[id]["business_obj"] = business
+    return info_dict
+
+# main
+consumer_key="8WhklGO_09rGkQZy9xMBQQ"
+consumer_secret="aSYhuXHB0U__JcYxWmBU-qm5a5k"
+token="ud25l_lGjq4PJPqxX_NXhSQHCbXWdZjq"
+token_secret="n4jTgD1PzwiWCSVg-hHac8WHx8o"
+
+keywords = "Pad See Ew"
+location = "San Francisco, CA"
+find_food(keywords, location)
+
+
